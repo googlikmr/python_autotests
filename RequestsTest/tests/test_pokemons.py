da1a632ff51080f9081ea3c4f58bb0910eec75e2
googlikmr <starodub007@gmail.com>
2024-12-19 03:29:12 +0300

    Update test_pokemons.py

--- RequestsTest/tests/test_pokemons.py
+++ RequestsTest/tests/test_pokemons.py
@@ -2,9 +2,9 @@ import requests
 import pytest
 
 URL = "https://api.pokemonbattle.ru/v2"
-TOKEN = "65ca50f7f4fd63714b9d19c3e6a1b220"
+TOKEN = "TOKEN" # put your token here
 HEADER = {"Content-Type" : "application/json", "trainer_token" : TOKEN}
-TRAINER_ID = "13471"
+TRAINER_ID = "ID" # put your trainer id here
 
 def test_status_code():
     respose_trainers = requests.get(url = f"{URL}/trainers", params = {"trainer_id" : TRAINER_ID})
